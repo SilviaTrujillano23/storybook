33e79d3a2dcdf605bfc1d915370ffc0af7ea0138
SilviaTrujillano23 <silvia_trujillano@mckinsey.com>
2023-10-30 12:18:14 -0500

    initial setup

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+// // Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+// module.exports = getDefaultConfig(__dirname);
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.resolverMainFields.unshift("sbmodern");
+
+module.exports = config;
